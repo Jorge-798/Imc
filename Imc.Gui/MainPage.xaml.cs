499ba78ec4166c10e434beb900d7a2d9f73f8e96
Jorge Mondragon Esparragoza <23700166@campeche.tecnm.mx>
2025-10-01 15:34:20 -0600

    Primera vercion de la interfaz de usuario

---
+++ Imc.Gui/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Imc.Gui
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+       
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Imc es una aplicacion .NET MAUI que permite calcular rl indice de masa corporal y determina el estado nutrisional de una persona
